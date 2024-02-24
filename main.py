2f45a61e35ef6ca39687c44ceaf762967c88ec14
cuthbertw1 <144268662+cuthbertw1@users.noreply.github.com>
2024-02-24 14:23:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,37 @@ import subprocess
 
 def backupDir(path, backupPath):
     if(os.path.exists(path) and os.path.exists(backupPath)):
-        subprocess.run(['cp','-r',path,backupPath])
+        mainpath=path
+        files=os.listdir(path)
+        for file in files:
+            currentPath=mainpath+'/'+file
+            subprocess.run(['cp',currentPath,backupPath])
     else:
         print('Error main path or backup path not found')
+def archiveDir(path, type):
+    validTypes=['zip','gztar','tar','bztar','xztar']
+
+    if os.path.exists(path):
+        if type == "zip":
+            subprocess.call(['zip','-r','/home/student/archiveddir.zip',path])
+
+
+
+def main():
+    print("Welcome user. What would you like to do?")
+    print("1. back up directory contents to new directory")         # initial choices
+    print("2. Archive the contents of a given directory")
+    print("5. Exit this menu")
+    userChoice=input()
+    if userChoice ==1:
+        path=input("enter path")
+        backuppath=input("enter backup path")       # runs backupDir function
+        backupDir(path,backuppath)
+    if userChoice==2:
+        path=input("Enter path: ")
+        type=input("Enter archive type: ")
+        archiveDir(path,type)
+    if userChoice==5:
+        pass                                        # exit condition
+
+main()
